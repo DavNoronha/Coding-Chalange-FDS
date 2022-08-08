26ce72fa313146ce13c2b2a1698bd4f1351ec38a
David Noronha Domingues <60622151+DavNoronha@users.noreply.github.com>
2022-08-08 08:44:17 -0300

    adding project to the repository

---
+++ calendar/layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-app>
+        <header>
+            <v-row>
+                <v-col class="d-flex justify-center align-center">
+                    <h1><strong>Calender</strong></h1>
+                </v-col>
+            </v-row>
+        </header>
+        <v-main>
+            <v-container>
+                <Nuxt />
+            </v-container>
+        </v-main>
+        <v-footer
+            :absolute="!fixed"
+            app
+        >
+            <span v-if="!showBackBtn">&copy; {{ new Date().getFullYear() }}</span>
+            <v-row class="py-5">                
+                <v-col
+                    v-if="showBackBtn"
+                    id="backBtn"
+                    class="d-flex justify-center align-center"
+                >
+                    <v-btn
+                        to="/"
+                        color="#4FC3F7"
+                    >
+                        Voltar
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-footer>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: 'DefaultLayout',
+    data () {
+        return {
+            fixed: false,
+        }
+    },
+    computed: {
+        showBackBtn() {
+            return this.$route.name == 'day-day' ? true : false
+        }
+    }
+}
+</script>
+
+<style scoped>
+header {
+    padding: 5px 0px;
+    background-color: #512DA8;
+}
+</style>
---
+++ calendar/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-row justify="center">
+        <v-col lg="11" class="d-flex justify-center">
+            <Calendar
+                :key="renderKey"
+                @reRender="renderKey++"
+            />
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+import Calendar from '@/components/templates/Calendar.vue';
+
+export default {
+    name: 'IndexPage',
+    components: { Calendar },
+    data() {
+        return {
+            renderKey: 0
+        }
+    }
+}
+</script>
